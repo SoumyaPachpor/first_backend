d2e1b3a4952e94ea76cea8fb39fbc83e6f9aa88a
SoumyaPachpor <busterhulk4734@gmail.com>
2024-07-13 19:30:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const db = require('./config/mongoose-connection')
+const cookieParser = require("cookie-parser");
+const productRouter = require("./routes/productRouter");
+const userRouter = require("./routes/userRouter")
+const ownerRouter = require("./routes/ownerRouter")
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/users', userRouter);
+app.use('/owner', ownerRouter);
+app.use('/products', productRouter);
+
+app.listen(port, () => {
+  console.log("Server is running on http://localhost:" + port);
+});
